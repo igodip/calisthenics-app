6e9ce0ab33ea26947d86ed45c7aac945d094c19b
Igor Maria Di Paolo <igor.di.paolo@gmail.com>
2026-01-21 12:24:53 +0100

    Refactor admin portal modules

---
+++ backend/admin/constants.js
@@ -0,0 +1,4 @@
+export const planStatuses = ['active', 'upcoming', 'draft', 'archived'];
+export const dayCodeOptions = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
+export const templateDayOptions = [2, 3, 4, 5];
+export const templateSlotsPerDay = 6;
