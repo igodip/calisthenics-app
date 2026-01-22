7540c0214345aea206e9b1064561a634d6b7e4d3
Igor Maria Di Paolo <igor.di.paolo@gmail.com>
2026-01-22 11:43:09 +0100

    Add week selection to plan builder

--- backend/admin/constants.js
+++ backend/admin/constants.js
@@ -1,5 +1,6 @@
 export const planStatuses = ['active', 'upcoming', 'draft', 'archived'];
 export const dayCodeOptions = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
 export const templateDayOptions = [1, 2, 3, 4, 5, 6, 7];
+export const templateWeekOptions = [1, 2, 3, 4, 5, 6, 8, 12];
 export const templateSlotsPerDay = 6;
 export const templateExerciseOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
